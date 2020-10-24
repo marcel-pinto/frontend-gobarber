316a37e0450241e1399a5692f2916678fc059bf2
Marcel Pinto <p.marcel.augusto@gmail.com>
2020-10-24 18:49:09 -0300

    Mock do hook de autenticacao

--- src/__tests__/pages/SignIn.spec.tsx
+++ src/__tests__/pages/SignIn.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 import SignIn from '../../pages/SignIn';
 
 const mockedHistoryPush = jest.fn();
@@ -11,8 +11,14 @@ jest.mock('react-router-dom', () => ({
   Link: ({ children } : { children: React.ReactNode}) => children,
 }));
 
+jest.mock('../../hooks/auth', () => ({
+  useAuth: () => ({
+    signIn: jest.fn(),
+  }),
+}));
+
 describe('SignIn Page', () => {
-  it('should be able to sign in', () => {
+  it('should be able to sign in', async () => {
     const { getByPlaceholderText, getByText } = render(<SignIn />);
 
     const emailField = getByPlaceholderText('E-mail');
@@ -24,6 +30,8 @@ describe('SignIn Page', () => {
 
     fireEvent.click(buttonElement);
 
-    expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard');
+    await waitFor(() => {
+      expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard');
+    });
   });
 });
