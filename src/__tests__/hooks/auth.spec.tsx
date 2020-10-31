af04e714b1706bcbb93d9690109722415e68a64a
Marcel Pinto <p.marcel.augusto@gmail.com>
2020-10-31 09:59:55 -0300

    Preparando a aplicacao

--- src/__tests__/hooks/auth.spec.tsx
+++ src/__tests__/hooks/auth.spec.tsx
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react-hooks';
 import MockAdapter from 'axios-mock-adapter';
 import { AuthProvider, useAuth } from '../../hooks/auth';
 import api from '../../services/api';
@@ -34,5 +34,64 @@ describe('Auth hook', () => {
     expect(setItemSpy).toHaveBeenCalledWith('@GoBarber:user', JSON.stringify(apiResposne.user));
 
     expect(result.current.user.email).toEqual('johndoe@example.com');
+  });
+
+  it('should restore save data from storage when auth inits', () => {
+    jest.spyOn(Storage.prototype, 'getItem').mockImplementation((key) => {
+      switch (key) {
+        case '@GoBarber:token':
+          return 'token-123';
+        case '@GoBarber:user':
+          return JSON.stringify({
+            id: 'user-id',
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+          });
+        default:
+          return null;
+      }
+    });
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+
+    expect(result.current.user.email).toEqual('johndoe@example.com');
+  });
+
+  it('should be able to sign out', async () => {
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+
+    const removeItem = jest.spyOn(Storage.prototype, 'removeItem');
+
+    act(() => {
+      result.current.signOut();
+    });
+
+    expect(removeItem).toHaveBeenCalledWith('@GoBarber:token');
+    expect(removeItem).toHaveBeenCalledWith('@GoBarber:user');
+  });
+
+  it('should be able to update user data', async () => {
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+
+    const setItemSpy = jest.spyOn(Storage.prototype, 'setItem');
+
+    const user = {
+      id: 'user-id',
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      avatar_url: 'image.jpg',
+    };
+
+    act(() => {
+      result.current.updateUser(user);
+    });
+
+    expect(setItemSpy).toHaveBeenCalledWith('@GoBarber:user', JSON.stringify(user));
+    expect(result.current.user).toEqual(user);
   });
 });
